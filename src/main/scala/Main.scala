7aca6f7c3b2056f63be001f6e08d327afeb5dc9b
Aristov Ivan <Apostrof1995@gmail.com>
2019-10-29 18:45:45 +0300

    Some code for fixing

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,15 +1,18 @@
 import scala.reflect.ClassTag
 
-import ReComposeTest._
-import cats.{Id, Monad}
+import cats.{Apply, Id, Monad}
 import cats.data.Kleisli
 import shapeless.ops.tuple.{Length, Prepend, Split}
-import shapeless.{Lazy, Nat, Succ, _0}
+import shapeless.{HNil, Lazy, Nat, Poly, Succ, _0}
 import shapeless.syntax.std.tuple._
 import shapeless.syntax.nat._
 import ReCompose._
-import cats.arrow.Compose
+import cats.arrow.{Arrow, Compose}
+import cats.effect.IO
 import cats.implicits._
+import shapeless.ops.tuple
+import cats.syntax.arrow.toArrowOps
+import cats.syntax.compose.toComposeOps
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -24,74 +27,184 @@ object Main {
     val e: String => Kleisli[Id, Boolean, Long] = _ => Kleisli[Id, Boolean, Long](_ => 0L)
     val f: String => Kleisli[Id, Long, String] = _ => Kleisli[Id, Long, String](_ => "1234")
 
-    trait Getter[F[_, _]] {
-      type G[_]
-      def get[A, B](f: F[A, G[B]])(a: A): G[B]
+//    def testProduct[T <: Product](value: T): T = value
+//
+//    testProduct("sd,jfb")
+
+    // A B C D
+    // 0 0 0 1
+    // 0 0 1 0
+    // 0 0 1 1
+    // 0 1 0 0
+    // 0 1 1 0
+    // 0 1 1 1
+    // 1 0 0 0
+    // 1 0 0 1
+    // 1 0 1 0
+    // 1 0 1 1
+    // 1 1 0 0
+    // 1 1 0 1
+    // 1 1 1 0
+    // 1 1 1 1
+
+
+    class SplitArrowed[F[_, _]] {
+      def apply[A, B <: Nat](arg: Split[A, B])(implicit F: Arrow[F]): F[A, arg.Out] = {
+        F.lift(arg(_: A))
+      }
+    }
+
+    class PrependArrowed[F[_, _]] {
+      def apply[A, B](arg: Prepend[A, B])(implicit F: Arrow[F]): F[(A, B), arg.Out] = {
+        F.lift { case (b, d) => arg(b, d) }
+      }
+    }
+
+    def arrowedS[F[_, _]]: SplitArrowed[F] = new SplitArrowed[F]
+
+    def arrowedP[F[_, _]]: PrependArrowed[F] = new PrependArrowed[F]
+
+    def tupled[F[_, _]: Arrow, A]: F[A, Tuple1[A]] = Arrow[F].lift(Tuple1(_: A))
+
+    def tupledR[F[_, _]: Arrow, A, B]: F[(A, B), (A, Tuple1[B])] = {
+      tupled[F, B].second[B]
+    }
+
+    def tupledL[F[_, _]: Arrow, A, B]: F[(A, B), (Tuple1[A], B)] = {
+      tupled[F, A].first[B]
+    }
+
+    implicit class toReComposeTupleOps2[A, B, F[_, _]: Arrow](f: F[A, B]) {
+      def &++&[C](g: F[A, C])(implicit P: Prepend[B, C]): F[A, P.Out] = {
+        (f &&& g) >>> arrowedP[F](P)
+      }
+
+      def &:+&[C](g: F[A, C])(implicit P: Prepend[B, Tuple1[C]]): F[A, P.Out] = {
+        (f &&& (g >>> tupled[F, C])) >>> arrowedP[F](P)
+      }
+
+      def &+:&[C](g: F[A, C])(implicit P: Prepend[Tuple1[B], C]): F[A, P.Out] = {
+        ((f >>> tupled[F, B]) &&& g) >>> arrowedP[F](P)
+      }
     }
 
 
-    implicit class toReComposeOps1[A: ClassTag, B, C, F[_, _]: Compose](f: A => F[B, C]) {
+    implicit class toReComposeTupleOps3[A <: Product, B <: Product, F[_, _]: Arrow](f: F[A, B]) {
+      //A, B, C, D - Product
+      def *+++*[C <: Product, D <: Product, N1 <: Nat, T1](g: F[C, D])(
+        implicit
+        L1: Length.Aux[A, N1],
+        P1: Prepend.Aux[A, C, T1],
+        P2: Prepend[B, D],
+        S1: Split.Aux[T1, N1, (A, C)]
+      ): F[T1, P2.Out] = {
+        arrowedP[F](P2) <<< (f *** g) <<< arrowedS[F](S1)
+      }
 
-      def >++>[A1: ClassTag, D, L <: Nat, P](g: A1 => F[C, D])(
+      //A, B, C - Product
+      def *+++*[C <: Product, D, N1 <: Nat, N2 <: Nat, T1, T2](g: F[C, D])(
+        implicit
+        L1: Length.Aux[A, N1],
+        L2: Length.Aux[B, N2],
+        P1: Prepend.Aux[A, C, T1],
+        P2: Prepend.Aux[B, Tuple1[D], T2],
+        S1: Split.Aux[T1, N1, (A, C)]
+      ): F[T1, T2] = {
+        arrowedS[F](S1) >>> (f *** g) >>> tupledR[F, B, D] >>> arrowedP[F](P2)
+      }
+
+      //A, B, D - Product
+      def *+++*[C, D <: Product, N1 <: Nat, N2 <: Nat, T1, T2](g: F[C, D])(
+        implicit
+        L1: Length.Aux[A, N1],
+        L2: Length.Aux[B, N2],
+        P1: Prepend.Aux[A, Tuple1[C], T1],
+        P2: Prepend.Aux[B, D, T2],
+        S1: Split.Aux[T1, N1, (A, C)]
+      ): F[T1, T2] = {
+        (f *** g) <<< tupledR[F, A, C]
+        arrowedS[F](S1) >>> (f *** g) >>> tupledR[F, A, C] >>> arrowedP[F](P2)
+      }
+
+      //A, B, C, D, - Product
+      /*def *+:+*[C, D, N1 <: Nat, T1](g: F[C, D])(
+        implicit
+        L1: Length.Aux[A, N1],
+        P1: Prepend.Aux[A, C, T1],
+        P2: Prepend[B, D],
+        S1: Split.Aux[T1, N1, (A, C)]
+      ): F[T1, P2.Out] = {
+        ((f *** g) <<< arrowed[F](S1)) >>> Arrow[F].lift[(B, D), P2.Out] { case (b, d) => P2(b, d) }
+      }*/
+
+//      def &:+&[C](g: F[A, C])(implicit P: Prepend[B, Tuple1[C]]): F[A, P.Out] = {
+//        (f &&& (g >>> Arrow[F].lift(Tuple1(_: C)))) >>> Arrow[F].lift[(B, Tuple1[C]), P.Out] { case (b, c) => P(b, c) }
+//      }
+//
+//      def &+:&[C](g: F[A, C])(implicit P: Prepend[Tuple1[B], C]): F[A, P.Out] = {
+//        ((f >>> Arrow[F].lift(Tuple1(_: B))) &&& g) >>> Arrow[F].lift[(Tuple1[B], C), P.Out] { case (b, c) => P(b, c) }
+//      }
+    }
+
+    // A A1
+    // 0 1
+    // 1 0
+    // 1 1
+
+    implicit class toReComposeTupleProductOps[A <: Product, B, C, F[_, _]: Arrow, G[_, _]](f: F[A, G[B, C]])(implicit RG: ReCompose[F, G]) {
+
+      def >++>[A1 <: Product, D, L <: Nat, P](g: F[A1, G[C, D]])(
         implicit
         L1: Length.Aux[A, L],
         P: Prepend.Aux[A, A1, P],
-        S: Split[P, L]
-      ): P => F[B, D] = p => S(p) match {
-        case (a: A, a1: A1) => Compose[F].compose(g(a1), f(a))
-      }
+        S: Split.Aux[P, L, (A, A1)]
+      ): F[P, G[B, D]] = (g <**< f) <<< arrowedS[F](S)
 
-      def >:+>[A1: ClassTag, D, L <: Nat, P](g: A1 => F[C, D])(
+      def >:+>[A1, D, L <: Nat, P](g: F[A1, G[C, D]])(
         implicit
         L1: Length.Aux[A, L],
         P: Prepend.Aux[A, Tuple1[A1], P],
-        S: Split[P, L]
-      ): P => F[B, D] = p => S(p) match {
-        case (a: A, a1: A1) => Compose[F].compose(g(a1), f(a))
-      }
+        S: Split.Aux[P, L, (A, A1)]
+      ): F[P, G[B, D]] = (g <**< f) <<< arrowedS[F](S)
 
-      def >+:>[A1: ClassTag, D, L <: Nat, P](g: A1 => F[C, D])(
+      def <++<[A1 <: Product, D, L <: Nat, P](g: F[A1, G[D, B]])(
         implicit
-        L1: Length.Aux[A, L],
-        P: Prepend.Aux[Tuple1[A], A1, P],
-        S: Split[P, L]
-      ): P => F[B, D] = p => S(p) match {
-        case (a: A, a1: A1) => Compose[F].compose(g(a1), f(a))
-      }
+        L1: Length.Aux[A1, L],
+        P: Prepend.Aux[A1, A, P],
+        S: Split.Aux[P, L, (A1, A)]
+      ): F[P, G[D, C]] = (f <**< g) <<< arrowedS[F](S)
+
+      def <:+<[A1, D, P](g: F[A1, G[D, B]])(
+        implicit
+        P: Prepend.Aux[Tuple1[A1], A, P],
+        S: Split.Aux[P, Nat._1, (A1, A)]
+      ): F[P, G[D, C]] = (f <**< g) <<< arrowedS[F](S)
     }
 
-    implicit class toReComposeOps2[A: ClassTag, B, C, F[_, _]: Compose, G[_]: Monad](f: Kleisli[G, A, F[B, C]]) {
+    implicit class toReComposeTupleOps[A, B, C, F[_, _]: Arrow, G[_, _]](f: F[A, G[B, C]])(implicit RG: ReCompose[F, G]) {
 
-      def >++>[A1: ClassTag, D, L <: Nat, P](g: Kleisli[G, A1, F[C, D]])(
+      def >+:>[A1 <: Product, D, P](g: F[A1, G[C, D]])(
         implicit
-        L1: Length.Aux[A, L],
-        P: Prepend.Aux[A, A1, P],
-        S: Split[P, L]
-      ): Kleisli[G, P, F[B, D]] = Kleisli(p => S(p) match {
-        case (a: A, a1: A1) => Monad[G].map2(g(a1), f(a))(Compose[F].compose)
-      })
-
-      def >:+>[A1: ClassTag, D, L <: Nat, P](g: Kleisli[G, A1, F[C, D]])(
-        implicit
-        L1: Length.Aux[A, L],
-        P: Prepend.Aux[A, Tuple1[A1], P],
-        S: Split[P, L]
-      ): Kleisli[G, P, F[B, D]] = Kleisli(p => S(p) match {
-        case (a: A, a1: A1) => Monad[G].map2(g(a1), f(a))(Compose[F].compose)
-      })
-
-      def >+:>[A1: ClassTag, D, L <: Nat, P](g: Kleisli[G, A1, F[C, D]])(
-        implicit
-        L1: Length.Aux[A, L],
         P: Prepend.Aux[Tuple1[A], A1, P],
-        S: Split[P, L]
-      ): Kleisli[G, P, F[B, D]] = Kleisli(p => S(p) match {
-        case (a: A, a1: A1) => Monad[G].map2(g(a1), f(a))(Compose[F].compose)
-      })
+        S: Split.Aux[P, Nat._1, (A, A1)]
+      ): F[P, G[B, D]] = (g <**< f) <<< arrowedS[F](S)
+
+      def <+:<[A1 <: Product, D, L <: Nat, P](g: F[A1, G[D, B]])(
+        implicit
+        L1: Length.Aux[A1, L],
+        P: Prepend.Aux[A1, Tuple1[A], P],
+        S: Split.Aux[P, L, (A1, A)]
+      ): F[P, G[D, C]] = (f <**< g) <<< arrowedS[F](S)
+
     }
 
-    val testing = (a >**> b) >++> (c1 >**> d1)
+    val testing0: ((String, String, Long, Boolean)) => String => Char = (b <**< a) >++> (d1 <**< c1)
 
+    val testing1: ((String, String, Long, Boolean)) => String => Char = (d1 <**< c1) <++< (b <**< a)
+
+    val testing2: ((String, String, String, String)) => Kleisli[Id, Int, String] = (d <**< c) >++> (f <**< e)
+
+    val testing3: ((String, String, String, String)) => Kleisli[Id, Int, String] = (f <**< e) <++< (d <**< c)
 
     ()
   }
--- src/main/scala/ReComposeTest.scala
+++ src/main/scala/ReComposeTest.scala
@@ -1,106 +1,91 @@
 
-import cats.FlatMap
-import cats.data.Kleisli
+import cats.arrow.Compose
+import cats.data.{Cokleisli, Kleisli}
+import cats.{Apply, Bimonad}
+import cats.syntax.apply.catsSyntaxApply
+import cats.syntax.compose.toComposeOps
+import cats.syntax.comonad.toComonadOps
+import cats.syntax.applicative.catsSyntaxApplicativeId
 
-object ReComposeTest {
-  type Kl[F[_], A, B] = Kleisli[F, A, B]
 
-  trait ReCompose[F[_, _], G[_, _]] {
-    def preCompose[A, B, C, D](f: G[B, C])(g: F[A, G[C, D]]): F[A, G[B, D]]
+trait ReCompose[F[_, _], G[_, _]] {
+  def preCompose[A, B, C, D]: G[C, D] => F[A, G[B, C]] => F[A, G[B, D]]
 
-    def preAndThen[A, B, C, D](f: F[A, G[B, C]])(g: G[C, D]): F[A, G[B, D]]
+  def preAndThen[A, B, C, D]: G[B, C] => F[A, G[C, D]] => F[A, G[B, D]]
 
-    def pairCompose[A, A1, B, C, D](f: F[A, G[C, D]])(g: F[A1, G[B, C]]): F[(A, A1), G[B, D]]
+  def pairCompose[A, A1, B, C, D]: F[A, G[C, D]] => F[A1, G[B, C]] => F[(A1, A), G[B, D]]
 
-    def pairAndThen[A, A1, B, C, D](f: F[A1, G[B, C]])(g: F[A, G[C, D]]): F[(A, A1), G[B, D]] = {
-      pairCompose[A, A1, B, C, D](g)(f)
-    }
+  def pairAndThen[A, A1, B, C, D]: F[A1, G[B, C]] => F[A, G[C, D]] => F[(A1, A), G[B, D]] = f => g => {
+    pairCompose(g)(f)
+  }
+}
 
+object ReCompose extends Instances {
+  def apply[F[_, _], G[_, _]](implicit instance: ReCompose[F, G]): ReCompose[F, G] = instance
+
+  implicit class toReComposeOps[F[_, _], G[_, _], A, X1, X2](f: F[A, G[X1, X2]])(implicit FG: ReCompose[F, G])  {
+    def pairAndThen[A1, X3](g: F[A1, G[X2, X3]]): F[(A, A1), G[X1, X3]] = FG.pairAndThen(f)(g)
+
+    /**
+     *     Alias for [[pairAndThen]]
+     */
+    def >**>[A1, X3](g: F[A1, G[X2, X3]]): F[(A, A1), G[X1, X3]] = FG.pairAndThen(f)(g)
+
+    def pairCompose[A1, X3](g: F[A1, G[X3, X1]]): F[(A1, A), G[X3, X2]] = FG.pairCompose(f)(g)
+
+    /**
+     * Alias for [[pairCompose]]
+     */
+    def <**<[A1, X3](g: F[A1, G[X3, X1]]): F[(A1, A), G[X3, X2]] = FG.pairCompose(f)(g)
+
+    def preCompose[X3](g: G[X2, X3]): F[A, G[X1, X3]] = FG.preCompose(g)(f)
+
+    /**
+     * Alias for [[preCompose]]
+     */
+    def <*<[X3](g: G[X2, X3]): F[A, G[X1, X3]] = FG.preCompose(g)(f)
+
+    def preAndThen[X3](g: G[X3, X1]): F[A, G[X3, X2]] = FG.preAndThen(g)(f)
+
+    /**
+     * Alias for [[preAndThen]]
+     */
+    def >*>[X3](g: G[X3, X1]): F[A, G[X3, X2]] = FG.preAndThen(g)(f)
+  }
+}
+
+trait Instances {
+
+  implicit def stdFunction1ReComposeInstance[F[_, _]: Compose]: ReCompose[? => ?, F] = new ReCompose[? => ?, F] {
+    def preCompose[A, B, C, D] = g => f => g <<< f(_)
+
+    def preAndThen[A, B, C, D] = g => f => f(_) <<< g
+
+    def pairCompose[A, A1, B, C, D] = g => f => { case (a1, a) => g(a) <<< f(a1) }
   }
 
-  object ReCompose {
-    def apply[F[_, _], G[_, _]](implicit instance: ReCompose[F, G]): ReCompose[F, G] = instance
+  implicit def stdKleislireComposeInstance[F[_]: Apply, G[_, _]: Compose]: ReCompose[Kleisli[F, ?, ?], G] = {
+    new ReCompose[Kleisli[F, ?, ?], G] {
 
-    implicit class toReComposeOps[F[_, _], G[_, _], A, X1, X2](f: F[A, G[X1, X2]])(implicit instance: ReCompose[F, G])  {
-      def pairAndThen[A1, X3](g: F[A1, G[X2, X3]]): F[(A1, A), G[X1, X3]] = instance.pairAndThen(f)(g)
+      def preCompose[A, B, C, D] = g => _.map(g <<< _)
 
-      def >**>[A1, X3](g: F[A1, G[X2, X3]]): F[(A1, A), G[X1, X3]] = instance.pairAndThen(f)(g)
+      def preAndThen[A, B, C, D] = g => _.map(_ <<< g)
 
-      def pairCompose[A1, X3](g: F[A1, G[X3, X1]]): F[(A, A1), G[X3, X2]] = instance.pairCompose(f)(g)
-
-      def <**<[A1, X3](g: F[A1, G[X3, X1]]): F[(A, A1), G[X3, X2]] = instance.pairCompose(f)(g)
-
-      def preCompose[X3](g: G[X3, X1]): F[A, G[X3, X2]] = instance.preCompose(g)(f)
-
-      def <*<[X3](g: G[X3, X1]): F[A, G[X3, X2]] = instance.preCompose(g)(f)
-
-      def preAndThen[X3](g: G[X2, X3]): F[A, G[X1, X3]] = instance.preAndThen(f)(g)
-
-      def >*>[X3](g: G[X2, X3]): F[A, G[X1, X3]] = instance.preAndThen(f)(g)
-    }
-  }
-
-  implicit val reComposeFuncFuncInstance: ReCompose[Function1, Function1] = {
-    new ReCompose[Function1, Function1] {
-      def pairCompose[A, A1, B, C, D](f: A => C => D)(g: A1 => B => C): ((A, A1)) => B => D = { case (a, a1) =>
-        f(a) compose g(a1)
-      }
-
-      def preCompose[A, B, C, D](f: B => C)(g: A => C => D): A => B => D = { a =>
-        g(a) compose f
-      }
-
-      def preAndThen[A, B, C, D](f: A => B => C)(g: C => D): A => B => D = { a =>
-        g compose f(a)
-      }
-    }
-  }
-
-  implicit def reComposeFuncKleisliInstance[Y[_]: FlatMap]: ReCompose[Function1, Kl[Y, ?, ?]] = {
-    new ReCompose[Function1, Kl[Y, ?, ?]] {
-      def pairCompose[A, A1, B, C, D](f: A => Kl[Y, C, D])(g: A1 => Kl[Y, B, C]): ((A, A1)) => Kl[Y, B, D] = {
-        case (a, a1) => f(a).compose(g(a1))
-      }
-
-      def preCompose[A, B, C, D](f: Kl[Y, B, C])(g: A => Kl[Y, C, D]): A => Kl[Y, B, D] = { a =>
-        g(a) compose f
-      }
-
-      def preAndThen[A, B, C, D](f: A => Kl[Y, B, C])(g: Kl[Y, C, D]): A => Kl[Y, B, D] = { a =>
-        g compose f(a)
-      }
-    }
-  }
-
-  implicit def reComposeKleisliFuncInstance[Y[_]](implicit Y: FlatMap[Y]): ReCompose[Kl[Y, ?, ?], Function1] = {
-    new ReCompose[Kl[Y, ?, ?], Function1] {
-      def preCompose[A, B, C, D](f: B => C)(g: Kl[Y, A, C => D]): Kl[Y, A, B => D] = {
-        Kleisli(a => Y.map(g.run(a))(_ compose f))
-      }
-
-      def preAndThen[A, B, C, D](f: Kl[Y, A, B => C])(g: C => D): Kl[Y, A, B => D] = {
-        Kleisli(a => Y.map(f.run(a))(g compose _))
-      }
-
-      def pairCompose[A, A1, B, C, D](f: Kl[Y, A, C => D])(g: Kl[Y, A1, B => C]): Kl[Y, (A, A1), B => D] = {
-        Kleisli { case (a, a1) => Y.map2(f.run(a), g.run(a1))(_ compose _) }
+      def pairCompose[A, A1, B, C, D] = g => f => {
+        Kleisli { case (a1, a) => g(a).map2(f(a1))(_ <<< _) }
       }
     }
   }
 
-  implicit def reComposeKleisliKleisliInstance1[Y[_]](implicit Y: FlatMap[Y]): ReCompose[Kleisli[Y, ?, ?], Kleisli[Y, ?, ?]] = {
-    new ReCompose[Kl[Y, ?, ?], Kl[Y, ?, ?]] {
-      def pairCompose[A, A1, B, C, D](f: Kl[Y, A, Kl[Y, C, D]])(g: Kl[Y, A1, Kl[Y, B, C]]): Kl[Y, (A, A1), Kl[Y, B, D]] = {
-        Kleisli { case (a, a1) => Y.map2(f.run(a), g.run(a1))(_ compose _) }
-      }
+  implicit def stdCokleisliReComposeInstance[F[_]: Bimonad, G[_, _]: Compose]: ReCompose[Cokleisli[F, ?, ?], G] = {
+    new ReCompose[Cokleisli[F, ?, ?], G] {
+      def preCompose[A, B, C, D] = g => _.map(g <<< _)
 
-      override def preCompose[A, B, C, D](f: Kl[Y, B, C])(g: Kl[Y, A, Kl[Y, C, D]]): Kl[Y, A, Kl[Y, B, D]] = {
-        Kleisli(a => Y.map(g.run(a))(_ compose f))
-      }
+      def preAndThen[A, B, C, D] = g => _.map(_ <<< g)
 
-      override def preAndThen[A, B, C, D](f: Kl[Y, A, Kl[Y, B, C]])(g: Kl[Y, C, D]): Kl[Y, A, Kl[Y, B, D]] = {
-        Kleisli(a => Y.map(f.run(a))(g compose _))
-      }
+      def pairCompose[A, A1, B, C, D] = g => f => Cokleisli(_.extract match {
+        case (a1, a) => g.run(a.pure[F]) <<< f.run(a1.pure[F])
+      })
     }
   }
 
